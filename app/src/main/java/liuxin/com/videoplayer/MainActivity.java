20ad8c2090b6adb386d87227364dabf69b21f72e
liuxin <liuxin2015@ipalfish.com>
2019-06-14 17:24:33 +0800

    hello  world

--- app/src/main/java/liuxin/com/videoplayer/MainActivity.java
+++ app/src/main/java/liuxin/com/videoplayer/MainActivity.java
@@ -1,13 +1,44 @@
 package liuxin.com.videoplayer;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.EventLogger;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends Activity {
+
+    private PlayerView mPlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlayerView = findViewById(R.id.exo_player);
+        initExoPlayer();
+    }
+
+    private void initExoPlayer() {
+        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this);
+        mPlayerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this,
+                "liuxin.com.videoplayer"));
+        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+                Uri.parse("http://b17.cdn.ipalfish.com/17/picturebook/51/f5/f8b954b144903a5c67c977029277")
+        );
+        player.prepare(videoSource);
+        player.addAnalyticsListener(new EventLogger(new DefaultTrackSelector()));
     }
 }
